590a50b4a93f1cb84e5b5911054de243d556a47f
sagnikmitra <sagnikmitra123@gmail.com>
2021-05-10 18:49:35 +0530

    init fb

---
+++ main.js
@@ -0,0 +1,61 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDRnb2yYf40EwaGmhByXr8kB9gewcJ4P-I",
+    authDomain: "fir-test-form-ef492.firebaseapp.com",
+    projectId: "fir-test-form-ef492",
+    storageBucket: "fir-test-form-ef492.appspot.com",
+    messagingSenderId: "73457706301",
+    appId: "1:73457706301:web:932ab1e9685d973eb49154",
+    measurementId: "G-8HG82ZFXDC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e) {
+    e.preventDefault();
+
+    // Get values
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+
+    // Save message
+    saveMessage(name, company, email, phone, message);
+
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+
+    // Hide alert after 3 seconds
+    setTimeout(function() {
+        document.querySelector('.alert').style.display = 'none';
+    }, 3000);
+
+    // Clear form
+    document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+        company: company,
+        email: email,
+        phone: phone,
+        message: message
+    });
+}
